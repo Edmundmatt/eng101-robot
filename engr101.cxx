060605fb9847a1a77739da54995d19e69fd8184c
Matthew Edmundson <mattedmundson47@gmail.com>
2019-05-14 14:25:13 +1200

    Update engr101.cxx

--- engr101.cxx
+++ engr101.cxx
@@ -45,38 +45,38 @@ class Robot{ //declaration
 };
 
 int main () { // example of main ( ) i n v o k i n g the c l a s s
-Robot robot;
-robot.InitHardware () ;
-int count = 0;
-open_screen_stream () ;
-while ( count < 5000)
-	{
-	robot.MeasureLine () ;
-	robot.FollowLine () ;
-	count ++;
-	}
-// s a v e _ p i ct u r e ( " f i l e 1 " ) ;
-close_screen_stream ( ) ;
-stoph() ;
-return 0;
+	Robot robot;
+	robot.InitHardware () ;
+	int count = 0;
+	open_screen_stream () ;
+	while ( count < 5000)
+		{
+		robot.MeasureLine () ;
+		robot.FollowLine () ;
+		count ++;
+		}
+	// s a v e _ p i ct u r e ( " f i l e 1 " ) ;
+	close_screen_stream ( ) ;
+	stoph() ;
+	return 0;
 }
 
 int Robot::FollowLine(){
-MeasureLine();
-if (line_present){
-dv = ( int ) (line_error∗kp) ;
-// dv = 0;
-v_left = v_left_go + dv ;
-v_right = v_right_go + dv ;
-cout<< "l i n e _ e r r o r = " << line_error << " dv= " <<dv ;
-SetMotors() ;
-} else {
-// go back
-cout<< " Line missing " <<endl ;
-v_left = 39;
-v_right = 55;
-SetMotors () ;
-sleep1( 100 ) ;
-}
-return 0;
+	MeasureLine();
+	if (line_present){
+		dv = ( int ) (line_error∗kp) ;
+		// dv = 0;
+		v_left = v_left_go + dv ;
+		v_right = v_right_go + dv ;
+		cout<< "l i n e _ e r r o r = " << line_error << " dv= " <<dv ;
+		SetMotors() ;
+	} else {
+		// go back
+		cout<< " Line missing " <<endl ;
+		v_left = 39;
+		v_right = 55;
+		SetMotors () ;
+		sleep1( 100 ) ;
+	}
+	return 0;
 }
