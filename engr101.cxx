9726f8264e05db8e6bb07d97cca3b788d4cdac95
longleonlong <50604756+longleonlong@users.noreply.github.com>
2019-05-14 14:22:08 +1200

    Update engr101.cxx

--- engr101.cxx
+++ engr101.cxx
@@ -21,63 +21,44 @@
  * 
  */
 
-#include <iostream>
-#include <math.h>
-#include <sys/time.h>
-#include "E101.h"
-
-using namespace std;
-int main(){
-struct timespec ts_start;
-struct timespec ts_end;
-clock_gettime(CLOCK_MONOTONIC, &ts_start);
-for ( int i = 0 ; i< 560;i++){//ci n t Robot : : Fo l lowL ine ( ) {
-MeasureLine ( ) ;
-i f ( l i n e _ p r e s e nt ) {
-dv = ( i n t ) ( l i n e _ e r r o r∗kp ) ;
-/ / dv = 0;
-v _ l e f t = v _ l eft _ g o + dv ;
-v _ r i g h t = v _ r i g ht _ g o + dv ;
-cout << " l i n e _ e r r o r = " << l i n e _ e r r o r << " dv= " <<dv ;
-SetMoto rs ( ) ;
-} else {
-/ / go back
-cout << " Line mi s sing " <<endl ;
-v _ l e f t = 39;
-v _ r i g h t = 55;
-SetMoto rs ( ) ;
-sleep1 ( 1 0 0 ) ;
-}
-re tu rn 0;
-}ode_to_time();
-double x = sin(i);
-}
-clock_gettime(CLOCK_MONOTONIC, &ts_end);
-long elapsed = (ts_end.tv_sec-ts_start.tv_sec)*1000000000 +
-ts_end.tv_nsec-ts_start.tv_nsec;
-cout<<"elapsed[ns]="<<elapsed<<endl;
+int main () { // example of main ( ) i n v o k i n g the c l a s s
+Robot robot;
+robot.InitHardware () ;
+int count = 0;
+open_screen_stream () ;
+while ( count < 5000)
+	{
+	robot.MeasureLine () ;
+	robot.FollowLine () ;
+	count ++;
+	}
+// s a v e _ p i ct u r e ( " f i l e 1 " ) ;
+close_screen_stream ( ) ;
+stoph() ;
+return 0;
 }
 
 class Robot{ //declaration
 	private:
-	int v_left,v_right, cam_tilt;
-	int dv;
-	double line_error;
-	int quadrant;
-	const int cam_width + 320;
-	const  int cam_height  =240;
+		int v_left,v_right, cam_tilt;
+		int dv;
+		double line_error;
+		int quadrant;
+		const int cam_width + 320;
+		const  int cam_height  =240;
+		
+		const int v_lefy_go =52; 
+		const int v_right_go = 43;
+		double kp = 0.05;
+		int line_present = 1;
 	
-	const int v_lefy_go =52; 
-	const int v_right_go = 43;
-	double kp = 0.05;
-	int line_present = 1;
 	public:
-	Rob(){};
-	int initHardware();
-	void ReadSetMotors();
-	void SetMotors();
-	int MeasureLine();
-	int FollowLine();
+		Rob(){};
+		int initHardware();
+		void ReadSetMotors();
+		void SetMotors();
+		int MeasureLine();
+		int FollowLine();
 };
 
 int Robot::FollowLine(){
