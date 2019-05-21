3d12e6575e1dfd55e55bc2e040aa6345784a6eb0
Matthew Edmundson <mattedmundson47@gmail.com>
2019-05-21 03:34:31 +0000

    Update AVC.cpp

--- AVC.cpp
+++ AVC.cpp
@@ -43,6 +43,8 @@ class Robot{ //declaration
 		const int v_right_go = 43;
 		double kp = 0.05;
 		int line_present = 1;
+		int leftThreshold = 150;
+		int rightThreshold = 170;
 	
 	public:
 		Robot(){};
@@ -53,24 +55,66 @@ class Robot{ //declaration
 		int MeasureLine();
 		int FollowLine();
 		void openGate();
+		void turnRight(int speed);
+		void turnLeft(int speed);
+		void goStraight();
 };
 
 int Robot::MeasureLine(){
 	int row = 120;
 	take_picture();
-	update_screen();
+	//update_screen();
 	
 	//Clear the array
 	for(int i = 0; i<320; i++){
 		fill_n(whiteness, i , 0);
 	}
-	
-	for(int col = 0; col< 320; col++){
+	//Testing loop for camera input
+	/*for(int col = 0; col< 320; col++){
 		int intensity = (int)get_pixel(row, col, 3);
 		whiteness[col] = intensity;
 		cout << whiteness[col];
-			
+		if (whiteness[col]< 80 ) {
+			cout << " _______ ";
+		}
+	}*/
+	int leftWhite = 0;
+	int black = 0;
+	int rightWhite = 0;
+	for(int col = 0; col<320; col++){
+		int intensity = (int)get_pixel(row,col, 3);
+		whiteness[col] = intensity;
 	}
+	for(int col = 0; col<320; col++){
+		while(whiteness[col] > 80){
+			leftWhite++;
+			col++;
+		}
+		while(whiteness[col] < 80){
+			black++;
+			col++;
+		}
+		while(whiteness[col] > 80){
+			rightWhite++;
+			col++;
+		}
+	}
+	
+	double ratio = (leftWhite-rightWhite);
+	int mArea = 20;
+	if(ratio > mArea){
+		//turn left
+		turnRight(5);
+	}
+	else if(ratio < -mArea){
+		//turn right
+		turnLeft(5);
+	}
+	else {
+		//go straight
+		goStraight();
+	}
+	
 	return 0;
 }
 
@@ -100,9 +144,9 @@ int Robot::FollowLine(){
 
 void Robot::openGate() {
 
-	char message[24]="Please";
+	char message[24]={'P','l','e','a','s','e'};
 	char password[24];
-	char IP[15]="130.195.6.196";
+	char IP[15]={'1','3','0','.','1','9','5','.','6','.','1','9','6'};
 
 	connect_to_server(IP, 1024); //need to find this out!!
 	send_to_server(message);
@@ -113,22 +157,28 @@ void Robot::openGate() {
 
 }
 
+void Robot::turnRight(int speed){
+	cout << "turning right ";
+}
+
+void Robot::turnLeft(int speed){
+	cout << "turning left ";
+}
+
+void Robot::goStraight(){
+	cout << "_________ ";
+}
+
 int main () { // example of main ( ) i n v o k i n g the c l a s s
+	int err = init(0);
 	open_screen_stream();
 	Robot robot;
 	robot.openGate();
-	robot.MeasureLine();
-	/*robot.initHardware () ;
-	//int count = 0;
-	open_screen_stream () ;
-	while ( count < 5000)
-		{
-		robot.FollowLine () ;
-		count ++;
-		}
-	// s a v e _ p i ct u r e ( " f i l e 1 " ) ;
-	close_screen_stream ( ) ;
-	stoph() ;*/
+	int count = 0;
+	while(count < 1000){
+		robot.MeasureLine();
+		count++;
+	}
 	close_screen_stream();
 	return 0;
 }
